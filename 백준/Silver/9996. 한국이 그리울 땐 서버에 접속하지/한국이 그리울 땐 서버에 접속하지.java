e0d265ec3737dce1a28ed9f0944eb6a31f603d90
choi <117348491+chanoChoi@users.noreply.github.com>
2023-01-30 17:49:13 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -3,16 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		String[] target = br.readLine().split("\\*");
+		String given = br.readLine();
+		int targetIdx = given.indexOf("*");
+		String prefix = given.substring(0, targetIdx);
+		String suffix = given.substring(targetIdx + 1);
+
 		StringBuilder sb = new StringBuilder();
 		while (N-- > 0) {
 			String tmp = br.readLine();
-			if (tmp.length() < (target[0] + target[1]).length()) {
+			if (tmp.length() < prefix.length() + suffix.length()) {
 				sb.append("NE");
-			}else if (tmp.startsWith(target[0]) && tmp.endsWith(target[1])) {
+			} else if (prefix.equals(tmp.substring(0, prefix.length())) && suffix.equals(
+				tmp.substring(tmp.length() - suffix.length()))) {
 				sb.append("DA");
 			} else {
 				sb.append("NE");
