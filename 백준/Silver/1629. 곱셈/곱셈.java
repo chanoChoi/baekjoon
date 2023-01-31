1893a85f883a30a65bdb651da6477f90ba568f0a
choi <117348491+chanoChoi@users.noreply.github.com>
2023-01-31 14:48:44 +0900

    [Silver I] Title: 곱셈, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] give = br.readLine().split(" ");
+		int A = Integer.parseInt(give[0]);
+		int B = Integer.parseInt(give[1]);
+		int C = Integer.parseInt(give[2]);
+
+		long l = helper(A, B, C);
+		System.out.println(l);
+	}
+
+	private static long helper(int a, int b, int c) {
+		if (b == 1) return a % c;
+		long num = helper(a, b / 2, c);
+		if ((b & 1) == 1) {
+			return (num * num % c) * a % c;
+		}
+		return num * num % c;
+	}
+}
