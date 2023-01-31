f107b601d48c27bd88f49bc91d1c17542eeff895
choi <117348491+chanoChoi@users.noreply.github.com>
2023-01-31 13:35:43 +0900

    [Silver IV] Title: 좋은 단어, Time: 220 ms, Memory: 23296 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = 0;
-        int N = Integer.parseInt(br.readLine());
-        while (N-- > 0) {
-            Deque<Character> stack = new LinkedList<>();
-            String str = br.readLine();
-            for (char c : str.toCharArray()) {
-                if (!stack.isEmpty() && stack.peekLast() == c) {
-                    stack.removeLast();
-                } else {
-                    stack.addLast(c);
-                }
-            }
-            if (stack.isEmpty()) {
-                cnt = cnt + 1;
-            }
-        }
-        System.out.println(cnt);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		while (N-- > 0) {
+			Deque<Character> stack = new LinkedList<>();
+			String give = br.readLine();
+			for (char each : give.toCharArray()) {
+				if (!stack.isEmpty() && stack.peekLast() == each) {
+					stack.removeLast();
+				} else {
+					stack.addLast(each);
+				}
+			}
+			if (stack.isEmpty()) {
+				cnt = cnt + 1;
+			}
+		}
+		System.out.print(cnt);
+	}
 }
