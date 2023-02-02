0642f9f3e329ea80b3d561c714ff9e6334c8f916
choi <117348491+chanoChoi@users.noreply.github.com>
2023-02-02 13:45:20 +0900

    [Silver I] Title: 안전 영역, Time: 276 ms, Memory: 18944 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
 	static int[] dx = {0, 1, 0, -1};
 	static int[] dy = {1, 0, -1, 0};
@@ -11,50 +10,39 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[][] map = new int[N][N];
-		boolean flag = false;
 		for (int i = 0; i < N; i++) {
 			String[] tmp = br.readLine().split(" ");
 			for (int j = 0; j < N; j++) {
 				int num = Integer.parseInt(tmp[j]);
 				map[i][j] = num;
 			}
-		}
-		int max = 1;
-		for (int i = 1; i < 101; i++) {
-			int[][] tmp = new int[N][N];
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < N; k++) {
-					if (map[j][k] <= i) {
-						tmp[j][k] = 0;
-					} else {
-						tmp[j][k] = 1;
-					}
-				}
-			}
-			int ret = 0;
+		} // 입력값 초기화
+		int ret = 1;
+		for (int h = 1; h < 101; h++) {
+			int cnt = 0;
 			boolean[][] visited = new boolean[N][N];
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < N; k++) {
-					if (tmp[j][k] == 1 && !visited[j][k]) {
-						ret = ret + 1;
-						dfs(j, k, visited, tmp);
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < N; y++) {
+					if (map[x][y] > h && !visited[x][y]) {
+						cnt = cnt + 1;
+						dfs(x, y, h, visited, map);
 					}
 				}
-			}
-			max = Math.max(max, ret);
+			}// count connected componet
+			ret = Math.max(ret, cnt);
 		}
-		System.out.println(max);
+		System.out.println(ret);
 	}
 
-	private static void dfs(int x, int y, boolean[][] visited, int[][] map) {
+	private static void dfs(int x, int y, int h, boolean[][] visited, int[][] map) {
 		visited[x][y] = true;
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length) continue;
-			if (map[nx][ny] == 0) continue;
+			if (map[nx][ny] <= h) continue;
 			if (visited[nx][ny]) continue;
-			dfs(nx, ny, visited, map);
+			dfs(nx, ny, h, visited, map);
 		}
 	}
 }
