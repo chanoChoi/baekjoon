277d53c7e4101439121bb9e076c729af001c426c
choi <117348491+chanoChoi@users.noreply.github.com>
2023-02-03 13:39:38 +0900

    [Silver I] Title: 쿼드트리, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,9 +1,10 @@
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.IOException;
 
 public class Main {
-	static char[][] map;
+    static char[][] map;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,27 +13,31 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			map[i] = br.readLine().toCharArray();
 		}
-		String ret= quard(0, 0, N);
-		System.out.println(ret);
+		quard(0, 0, N);
+		System.out.print(sb.toString());
 	}
 
-	private static String quard(int y, int x, int size) {
-		if (size == 1) return String.valueOf(map[y][x]);
-		StringBuilder ret = new StringBuilder();
-		char b = map[y][x];
+	private static void quard(int y, int x, int size) {
+		char cur = map[y][x];
+		if (size == 1) {
+			sb.append(map[y][x]);
+			return;
+		}
+
 		for (int i = y; i < y + size; i++) {
 			for (int j = x; j < x + size; j++) {
-				if (map[i][j] != b) {
-					ret.append('(');
-					ret.append(quard(y, x, size / 2));
-					ret.append(quard(y, x + size / 2, size / 2));
-					ret.append(quard(y + size / 2, x, size / 2));
-					ret.append(quard(y + size / 2, x + size / 2, size / 2));
-					ret.append(')');
-					return ret.toString();
+				if (cur != map[i][j]) {
+					int half = size / 2;
+					sb.append('(');
+					quard(y, x, half);
+					quard(y, x + half, half);
+					quard(y + half, x, half);
+					quard(y + half, x + half, half);
+					sb.append(')');
+					return;
 				}
 			}
 		}
-		return String.valueOf(map[y][x]);
+		sb.append(map[y][x]);
 	}
 }
