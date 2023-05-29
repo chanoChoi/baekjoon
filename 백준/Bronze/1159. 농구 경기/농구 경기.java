c8d4d2e7d6c473ebd8f4e2b30073f1312f9262d6
choi <117348491+chanoChoi@users.noreply.github.com>
2023-05-29 16:42:06 +0900

    [Bronze II] Title: 농구 경기, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.java
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] cnt = new int[26];
+        Map<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             char ch = br.readLine().charAt(0);
-            cnt[ch - 'a']++;
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
         boolean flag = false;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < cnt.length; i++) {
-            int cur = cnt[i];
-            if (cur >= 5) {
-                sb.append((char) (i + 'a'));
+        for (Map.Entry<Character, Integer> item : map.entrySet()) {
+            if (item.getValue() >= 5) {
+                sb.append(item.getKey());
                 flag = true;
             }
         }
