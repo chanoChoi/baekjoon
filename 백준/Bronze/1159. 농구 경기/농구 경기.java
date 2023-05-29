7c9b5d540b2f1298a3208bf0637fd9592b68131e
choi <117348491+chanoChoi@users.noreply.github.com>
2023-05-29 16:36:38 +0900

    [Bronze II] Title: 농구 경기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.java
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -3,24 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] cnt = new int[26];
-        int N = Integer.parseInt(br.readLine());
-        while (N-- > 0) {
-            char c = br.readLine().charAt(0);
-            cnt[c - 'a']++;
+
+        for (int i = 0; i < n; i++) {
+            char ch = br.readLine().charAt(0);
+            cnt[ch - 'a']++;
         }
+
+        boolean flag = false;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < cnt.length; i++) {
-            if (cnt[i] >= 5) {
+            int cur = cnt[i];
+            if (cur >= 5) {
                 sb.append((char) (i + 'a'));
+                flag = true;
             }
         }
-        if (sb.length() == 0) {
-            System.out.print("PREDAJA");
-        } else {
-            System.out.print(sb.toString());
+
+        if (!flag) {
+            sb.append("PREDAJA");
         }
-	}
+
+        System.out.print(sb.toString());
+    }
 }
